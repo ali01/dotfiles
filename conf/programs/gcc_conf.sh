99f54990ed046499c5b4cf622dccf272f2e36331
Ali Yahya <alive@ali01.com>
2010-05-13 13:35:44 -0700

    general updates

--- conf/programs/gcc_conf.sh
+++ conf/programs/gcc_conf.sh
@@ -1,3 +1,4 @@
-I1=/vol/usr/include
-export C_INCLUDE_PATH="$I1"
-export CPLUS_INCLUDE_PATH="$I1"
+I1='/vol/usr/include'
+I2='/opt/local/include'
+export C_INCLUDE_PATH="$I2:$I1"
+export CPLUS_INCLUDE_PATH="$I2:$I1"
--- conf/programs/ld_conf.sh
+++ conf/programs/ld_conf.sh
@@ -1,2 +1,5 @@
-export VOL_LIBRARY_PATH="/vol/usr/lib"
-export LIBRARY_PATH="$VOL_LIBRARY_PATH:$LIBRARY_PATH"
+# export VOL_LIBRARY_PATH="/vol/usr/lib"
+if [[ $mac_os == true ]] ; then
+  export MACPORTS_LIBRARY_PATH="/opt/local/lib"
+  export LIBRARY_PATH="$MACPORTS_LIBRARY_PATH:$LIBRARY_PATH"
+fi
