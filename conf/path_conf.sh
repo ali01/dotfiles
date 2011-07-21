89223b8d3bb199b368af3435ff35f8a1dbdec46c
Ali Yahya <ali01@ali01.com>
2011-07-20 21:51:37 -0700

    changes

--- conf/path_conf.sh
+++ conf/path_conf.sh
@@ -7,14 +7,14 @@ export path_google_depot_tools="$code/research/starlight/google_src/depot_tools"
 
 typeset -U path
 path=($path_ali01 $path_vol $path_google_depot_tools $path)
-path=('/opt/local/bin' '/opt/local/sbin' $path) # macports
+path=('/usr/local/sbin' $path) # brew
 
-typeset -U cdpath
-cdpath=(. $code "$code/academic")
+typeset -U manpath
+manpath=('/vol/usr/man' $manpath)
+
+# typeset -U cdpath
+# cdpath=(. $code "$code/academic")
 
 typeset -U fpath
 fpath=($zshfn/**/ $fpath)
 
-typeset -U manpath
-manpath=('/opt/local/share/man' '/vol/usr/man' $manpath)
-manpath=('/opt/local/share/man' $manpath) # macports
