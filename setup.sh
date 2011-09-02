b0f384a37fb56e07b3b4572af8d6989b828f531a
Ali Yahya <ali01@ali01.com>
2011-09-02 16:32:46 -0700

    env config

--- setup.sh
+++ setup.sh
@@ -2,8 +2,16 @@
 
 DOTFILES_DIR=`pwd`
 
+# Creating top-level dotfile symlinks
 for dotfile in `ls -A $DOTFILES_DIR | grep -vw .git | grep -E "^[.].*"`; do
   ln -sf $DOTFILES_DIR/$dotfile $HOME
 done
 
+# Creating zsh dotfile symlinks
 ln -sf $DOTFILES_DIR/zsh/.z* $HOME
+
+# Configuring environment
+mkdir -p ~/home
+mkdir -p ~/.ssh/master
+touch ~/.ssh/known_hosts
+cp -n $DOTFILES_DIR/ssh/config-base ~/.ssh/config
