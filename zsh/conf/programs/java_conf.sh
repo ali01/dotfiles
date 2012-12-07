89a14d8d61e8c6844defeb98289816c8c748124c
Ali Yahya <git@outlast.me>
2012-12-07 04:04:26 -0800

    JAVA_HOME

--- zsh/conf/programs/java_conf.sh
+++ zsh/conf/programs/java_conf.sh
@@ -1,3 +1,3 @@
 if [[ $mac_os == true ]] ; then
-  export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home'
+  export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 fi
