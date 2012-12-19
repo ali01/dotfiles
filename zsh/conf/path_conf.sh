d84afdaa8650e5a3d201cf76bd51fc3b34b1be56
Ali Yahya <git@outlast.me>
2012-12-18 18:00:37 -0800

    zsh path

--- zsh/conf/path_conf.sh
+++ zsh/conf/path_conf.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
 export path_user='~/bin'
-export path_vol='/vol/usr/bin'
 export path_ali01="$shell/0-path"
 export path_brew='/usr/local/sbin'
+export path_npm='/usr/local/share/npm/bin'
 
 typeset -U path
-path=($path_user $path_ali01 $path_brew $path_vol $path)
+path=($path_user $path_ali01 $path_brew $path_npm $path)
 
 typeset -U manpath
-manpath=('/vol/usr/man' $manpath)
+manpath=('/usr/local/share/man' $manpath)
 
 # typeset -U cdpath
 # cdpath=(. $code "$code/academic")
