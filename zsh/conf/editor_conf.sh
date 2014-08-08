851ff94b92e4a12a17fed48609864a3f3ea9f494
Ali Yahya <git@outlast.me>
2014-08-08 16:14:43 -0700

    shell editor variables

--- zsh/conf/editor_conf.sh
+++ zsh/conf/editor_conf.sh
@@ -2,12 +2,6 @@
 
 alias emacs='emacs -nw'
 
-if [[ $linux == true ]] ; then
-   export GIT_EDITOR='emacs'
-   export VISUAL='emacs'
-   export EDITOR='emacs'
-elif [[ $mac_os == true ]] ; then
-   export GIT_EDITOR='mate -w'
-   export VISUAL='mate -w'
-   export EDITOR='mate -w'
-fi
+export GIT_EDITOR='emacs'
+export VISUAL='emacs'
+export EDITOR='emacs'
