4912460a8f7c9444a8eed94cee8e7546f000eeff
Ali Yahya <git@outlast.me>
2014-08-20 17:00:31 -0700

    gopath

--- zsh/conf/path_conf.sh
+++ zsh/conf/path_conf.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+export GOPATH="/home/alive/home/code/go"
+export go_base='/usr/local/go'
+
 typeset -U path
-path=('/usr/local/sbin' $path)
+path=('/usr/local/sbin' $go_base/bin $GOPATH/bin $path)
 
 typeset -U manpath
 manpath=('/usr/local/share/man' $manpath)
