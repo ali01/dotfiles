bff86d95eab279fd626c60460dddcd612d2c9e98
Ali Yahya <alive@outlast.me>
2015-05-11 14:16:26 -0700

    removed macports paths

--- zsh/conf/programs/gcc_conf.sh
+++
@@ -1,4 +0,0 @@
-I1='/vol/usr/include'
-I2='/opt/local/include'
-export C_INCLUDE_PATH="$I2:$I1"
-export CPLUS_INCLUDE_PATH="$I2:$I1"
--- zsh/conf/programs/ld_conf.sh
+++
@@ -1,5 +0,0 @@
-# export VOL_LIBRARY_PATH="/vol/usr/lib"
-if [[ $mac_os == true ]] ; then
-  export MACPORTS_LIBRARY_PATH="/opt/local/lib"
-  export LIBRARY_PATH="$MACPORTS_LIBRARY_PATH:$LIBRARY_PATH"
-fi
